578c13973c8aef2bd03c8cb1b6e7ec812f2b90a5
Vilius Andraitis <vilius.andraitis@gmail.com>
2024-03-17 08:58:04 +0200

    add transparency to colorscheme

--- init.lua
+++ init.lua
@@ -736,6 +736,13 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
     'folke/tokyonight.nvim',
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = 'transparent',
+        floats = 'transparent',
+      },
+    },
     priority = 1000, -- make sure to load this before all the other start plugins
     init = function()
       -- Load the colorscheme here.
