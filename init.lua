12b5cc14ca4e3157d1b5b605800ead002538f03d
Vilius Andraitis <vilius.andraitis@gmail.com>
2024-07-11 07:58:17 +0300

    something

--- init.lua
+++ init.lua
@@ -809,7 +809,6 @@ require('lazy').setup({
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
-  { 'lukas-reineke/indent-blankline.nvim', main = 'ibl', opts = {} },
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- put them in the right spots if you want.
